5df6e567c68c4d4c49ee991afd9fddbe55d1ee49
Sam Naish <sam.naish92@gmail.com>
2021-08-20 11:59:23 +0100

    feat: added post endpoint for query params, Note: post method is playing nice, so will look into that.

--- handler.js
+++ handler.js
@@ -1,13 +1,31 @@
-'use strict';
-
-module.exports.helloWorld = async () => {
-  return {
+exports.helloWorld = async (event) => {
+  console.log('request successful!');
+  const rawDateString = new Date();
+  const neatDate = rawDateString.toLocaleDateString();
+  const time = rawDateString.toLocaleTimeString();
+  const response = {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Hello World, I have done it!',
+        date: neatDate,
+        time: time,
+        input: event.queryStringParameters
+      }
+    )
+  };
+  return response;
+};
+
+exports.getParams = async (event) => {
+  const query = event.queryStringParameters;
+  return {
+    statusCode: 201,
+    body: JSON.stringify(
+      {
+        message: `This is the POST endpoint.`,
+        input: query
       }
     )
   };
 };
-
